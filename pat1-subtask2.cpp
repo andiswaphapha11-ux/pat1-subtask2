23886580d608390a7df377ec349e203d45ca98ff
Andiswa <andiswaphapha11@gmail.com>
2026-04-20 11:32:18 +0200

    Create pat1-subtask2.cpp

---
+++ pat1-subtask2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int firstTemp, secondTemp, thirdTemp;
+    int difference;
+
+    // Input first temperature
+    cout << "Enter the first temperature reading: ";
+    cin >> firstTemp;
+
+    
